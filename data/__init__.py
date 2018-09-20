fa8fea4bdf247898002b65fe720f89e1fe16ae5b
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-09-20 13:25:09 -0400

    factoring out data

---
+++ data/__init__.py
@@ -0,0 +1,23 @@
+from collections import namedtuple
+
+from sklearn import datasets
+
+Dataset = namedtuple("Dataset", ["name", "data", "target"])
+
+def make_sklearn_dataset(name, loader):
+	data = loader()
+	return Dataset(name, data.data, data.target)
+
+datasets = [
+    make_sklearn_dataset("boston", datasets.load_boston),
+    make_sklearn_dataset("iris", datasets.load_iris),
+    make_sklearn_dataset("diabetes", datasets.load_diabetes),
+    make_sklearn_dataset("digits", datasets.load_digits),
+    make_sklearn_dataset("linnerud", datasets.load_linnerud),
+    make_sklearn_dataset("wine", datasets.load_wine),
+    make_sklearn_dataset("breast_cancer", datasets.load_breast_cancer),
+]
+
+def dataset_iter():
+	for dataset in datasets:
+		yield dataset
--- test.py
+++ test.py
@@ -1,29 +1,21 @@
 from sklearn.model_selection import cross_val_score
-from sklearn import datasets
 from sklearn.linear_model import LinearRegression
 
-LOADERS = [
-    datasets.load_boston,
-    datasets.load_iris,
-    datasets.load_diabetes,
-    datasets.load_digits,
-    datasets.load_linnerud,
-    # datasets.load_wine,
-    datasets.load_breast_cancer,
-]
-
-def _dataset_iter():
-	for loader in LOADERS:
-		yield loader()
+from data import dataset_iter
 
 def benchmark_estimator(Estimator, **kwargs):
 	est = Estimator()
-	for data in _dataset_iter():
-		scores = cross_val_score(est, data.data, data.target, **kwargs)
+	for dataset in dataset_iter():
+		scores = cross_val_score(est, dataset.data, dataset.target, **kwargs)
 		print
-		print scores
+		print {
+			"dataset": dataset.name,
+			"estimator": est.__class__.__name__,
+			"scores": list(scores),
+			"kwargs": kwargs,
+		}
 		print
 
 def test(capsys):
 	with capsys.disabled():
-		benchmark_estimator(LinearRegression)
+		benchmark_estimator(LinearRegression, cv=5)
